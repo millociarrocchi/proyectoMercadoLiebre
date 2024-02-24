75e9de63b3042d4cf03579a1849c087259e37670
millociarrocchi <deliveryeduardo21@gmail.com>
2024-02-23 20:43:49 -0500

    desafio vista final

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ app.listen(3055, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+});
+
+app.post('/register', (req, res) => {
+    res.send("Se registro con exito")
+});
+
+app.get("/login", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
+app.post("/login", function(req, res){
+    res.send("Bienvenido")
+})
